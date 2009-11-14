e2269d91d0e8422e97f3f839eb87a69769f7204c
reddavis <reddavis@gmail.com>
2009-11-14 20:33:15 +0000

    initial commit

---
+++ lib/naive_bayes.rb
@@ -0,0 +1,80 @@
+# Bayes Theorem
+# P(A|B) = P(B|A) * P(A) / P(B)
+
+# Terminology
+# An ITEM is made up of FEATURES
+# An ITEM belongs to a CLASS
+
+# Bayes With Our Terminology
+# P(Class | Item) = P(Item | Class) * P(Class) / P(Item)
+
+# However, when classifying, P(Item) is the same across all calcualtions
+# So we don't bother to calculate it
+
+class NaiveBayes
+  
+  attr_reader :features_count, :klass_count
+  
+  def initialize(*klasses)
+    @features_count = {}
+    @klass_count = {}
+    @klasses = klasses
+    
+    klasses.each do |klass|
+      @features_count[klass] = Hash.new(0.0)
+      @klass_count[klass] = 0.0
+    end
+  end
+  
+  def train(klass, *features)
+    features.uniq.each do |feature|
+      @features_count[klass][feature] += 1
+    end
+    @klass_count[klass] += 1
+  end
+  
+  #P(Class | Item) = P(Item | Class) * P(Class)
+  def classify(*features)
+    scores = {}
+    @klasses.each do |klass|
+      scores[klass] = (prob_of_item_given_a_class(features, klass) * prob_of_class(klass))
+    end
+    scores.sort {|a,b| b[1] <=> a[1]}[0]
+  end
+  
+  private
+  
+  # P(Item | Class)
+  def prob_of_item_given_a_class(features, klass)
+    a = features.inject(1.0) do |sum, feature|
+      prob = prob_of_feature_given_a_class(feature, klass)
+      prob = assumed_probability if prob == 0
+      sum *= prob
+    end
+  end
+  
+  # P(Feature | Class)
+  def prob_of_feature_given_a_class(feature, klass)
+    return 0.5 if @klass_count[klass] == 0
+    @features_count[klass][feature] / @klass_count[klass]
+  end
+  
+  # P(Class)
+  def prob_of_class(klass)
+    @klass_count[klass] / total_items
+  end
+  
+  def total_items
+    @klass_count.inject(0) do |sum, klass|
+      sum += klass[1]
+    end
+  end
+  
+  # If we have only trained a little bit a class may not have had a feature yet
+  # give it a probability of 0 may not be true so we produce a assumed probability
+  # which gets smaller more we train
+  def assumed_probability
+    0.5 / (total_items/2)
+  end
+  
+end
--- spec/naive_bayes_spec.rb
+++ spec/naive_bayes_spec.rb
@@ -1,7 +1,44 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "NaiveBayes" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  
+  describe "Initialization" do
+    before(:all) do
+      @classifier = NaiveBayes.new(:spam, :ham)
+    end
+    
+    it "should create a features count for each class" do
+      @classifier.features_count.size.should == 2
+    end
   end
+  
+  describe "Training" do
+    before(:all) do
+      @classifier = NaiveBayes.new(:spam, :ham)
+      @classifier.train(:spam, 'bad', 'word')
+    end
+        
+    it "should train" do
+      @classifier.features_count[:spam].size.should == 2
+    end
+    
+    it "should bump klass_count for spam up to 1" do
+      @classifier.klass_count[:spam].should == 1
+    end
+  end
+  
+  describe "Classification" do
+    before do
+      @classifier = NaiveBayes.new(:spam, :ham)
+      @classifier.train(:spam, 'bad', 'word')
+      @classifier.train(:ham, 'we', 'bad')
+    end
+    
+    it "should" do
+      a = @classifier.classify('bad', 'word')
+      a[0].should == :spam
+      a[1].should == 0.5
+    end
+  end
+  
 end
